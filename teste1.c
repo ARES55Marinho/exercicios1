7969f4d12d49bd5ede13ebc763fc72ac4360bd6e
Hiago Marinho <multiagranel2024@gmail.com>
2026-09-02 10:24:03 -0300

    Add files via upload

---
+++ teste1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    float nota1, nota2, media;
+
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+
+    media = (nota1 + nota2) / 2;
+
+    printf("Sua media foi: %.2f\n", media);
+
+    if (media >= 5.0) {
+        printf("Aprovado!\n");
+    } else if (media >= 3) {
+    	printf("Exame!\n");
+	} else {
+        printf("Reprovado!\n");
+    }
+
+    return 0;
+}
